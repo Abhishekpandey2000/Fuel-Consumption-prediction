360dee4a01fec0b8915f9bed6c344f5841cc693e
Abhi1683 <118616156+Abhi1683@users.noreply.github.com>
2023-02-19 03:13:12 +0530

    Update fuel_prediction.py

--- fuel_prediction.py
+++ fuel_prediction.py
@@ -3,6 +3,33 @@ import pandas as pd
 import pickle as pk
 import streamlit as st
 
+import requests  # pip install requests
+import streamlit as st  # pip install streamlit
+from streamlit_lottie import st_lottie  # pip install streamlit-lottie
+
+
+def load_lottieurl(url: str):
+    r = requests.get(url)
+    if r.status_code != 200:
+        return None
+    return r.json()
+    
+
+# lottie_coding = load_lottiefile("lottiefile.json")  # replace link to local lottie file
+lottie_hello = load_lottieurl("https://assets7.lottiefiles.com/packages/lf20_caijenqq.json")
+
+st_lottie(
+    lottie_hello,
+    speed=1,
+    reverse=False,
+    loop=True,
+    quality="low", # medium ; high
+    renderer="svg", # canvas
+    height=None,
+    width=None,
+    key=None,
+)
+
 loaded_model = pk.load(
     open("trained_model_rf.sav","rb"))
 scaled_data = pk.load(
