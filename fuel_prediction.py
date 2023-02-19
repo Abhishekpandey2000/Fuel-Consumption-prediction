3748b49d07b880be8a9af42f05bba1edc7205d55
Abhi1683 <118616156+Abhi1683@users.noreply.github.com>
2023-02-20 00:06:23 +0530

    Update fuel_prediction.py

--- fuel_prediction.py
+++ fuel_prediction.py
@@ -60,12 +60,11 @@ def main():
     transmission = ['AV', 'AM', 'M', 'AS', 'A']
     fuel = ["D", "E", "X", "Z"]
     Vehicle_class = st.selectbox("Select the vehicle class",vehicle)
-    Engine_size = st.number_input("Enter Engine Size")
-    Cylinders = st.number_input("Enter number of Cylinders")
+    Engine_size = st.number_input("Enter Engine Size (please enter value in this range[1-7])")
+    Cylinders = st.number_input("Enter number of Cylinders (please enter value in this range[1-16]",min_value = 1, max_value = 16)
     Transmission = st.selectbox("Select the Transmission",transmission)
-    Co2_Rating = st.number_input("Enter CO2 Rating")
+    Co2_Rating = st.number_input("Enter CO2 Rating (please enter value in this range[1-10]",min_value = 1, max_value = 10)
     Fuel_type = st.selectbox("Select the Fuel type",fuel)
-
     # code for prediction
 
     # creating a button for prediction
