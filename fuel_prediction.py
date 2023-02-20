fb9a86c42372c1b5cdd3504c99099f12fde8c38d
Abhi1683 <118616156+Abhi1683@users.noreply.github.com>
2023-02-20 16:16:30 +0530

    Update fuel_prediction.py

--- fuel_prediction.py
+++ fuel_prediction.py
@@ -100,7 +100,7 @@ def main():
     
     _left, mid, _right = st.columns(3)
     with mid:
-       st.image("output-onlinegiftools.gif",width = 150)
+       st.image("output-onlinegiftools.gif")
     
     st.markdown("<h1 style='text-align: center; color: red;'>Fuel Consumption Prediction</h1>", unsafe_allow_html=True)
     
