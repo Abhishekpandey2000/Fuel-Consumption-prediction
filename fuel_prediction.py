af8f9a5324504479f1866684031264f13c401785
Abhishek Pandey <118616156+Abhishekpandey2000@users.noreply.github.com>
2023-04-07 21:12:11 +0530

    Update fuel_prediction.py

--- fuel_prediction.py
+++ fuel_prediction.py
@@ -24,7 +24,7 @@ def get_img_as_base64(file):
 page_bg_img = f"""
 <style>
 [data-testid="stAppViewContainer"] > .main {{
-background-image: url("https://cdna.artstation.com/p/assets/images/images/028/796/598/large/christina-pigeon-movie006-1-moment.jpg?1595529009");
+background-image: url("https://wallpapercrafter.com/th800/245197-a-long-exposure-shot-of-a-dark-empty-road-with-mot.jpg");
 background-size: 150%;
 background-position: top left;
 background-repeat: no-repeat;
